7cf298e12f5a1f6c3aa4211c55070307d5335f04
Rasheduzzaman <rashedzaman1978@gmail.com>
2026-05-02 07:21:00 +0600

    proxy file added and fix baseURL in auth-client.js file

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,5 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "https://batch-13-assignment-8-online-book-b.vercel.app"
+  // baseURL: "https://batch-13-assignment-8-online-book-b.vercel.app"
+  baseURL: "http://localhost:3000"
 });
