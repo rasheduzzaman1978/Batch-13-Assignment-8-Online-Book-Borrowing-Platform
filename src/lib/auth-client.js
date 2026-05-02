96822a9b962f2c34d91223a11dfd04ec6df58812
Rasheduzzaman <rashedzaman1978@gmail.com>
2026-05-02 07:43:21 +0600

    auth-client file updated

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  // baseURL: "https://batch-13-assignment-8-online-book-b.vercel.app"
-  baseURL: "http://localhost:3000"
+  baseURL: 
+    process.env.NODE_ENV === "production"
+      ? "https://batch-13-assignment-8-online-book-b.vercel.app"
+      : "http://localhost:3000",
 });
